361ab0fd634e0a912d381c7aeb475364313e1db8
kelly tastets <kelly.tastets@gmail.com>
2020-02-29 12:25:58 +0100

    rectif gif dubuffet

--- projets/page-projets.php
+++ projets/page-projets.php
@@ -13,7 +13,7 @@
     <a href="projets/dubuffet.php" class="photoProjet2">
         <h3 class="mobileProjectTitle">LA PETITE FABRIQUE DE DUBUFFET</h3>
         <img class="vignetteProjet" src="assets/img/projets/9dubuffet/image_preview.jpg" alt="">
-        <img class="hover" src="assets/img/projets/9dubuffet/gifBA.gif">
+        <img class="hover" src="assets/img/projets/9dubuffet/gifJaune.gif">
     </a>
     <a href="projets/scenarioCru.php" class="photoProjet3">
         <h3 class="mobileProjectTitle">LA VILLE EN JEUX / 12M3</h3>
